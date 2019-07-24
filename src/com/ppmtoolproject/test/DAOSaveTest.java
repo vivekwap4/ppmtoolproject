db1d9d34d4ff0c39aa2ca5f5bbf516c518b45bc1
willerth <willerth@iastate.edu>
2019-07-24 11:49:06 -0500

    added dao

---
+++ src/com/ppmtoolproject/test/DAOSaveTest.java
@@ -0,0 +1,19 @@
+package com.ppmtoolproject.test;
+
+import com.ppmtoolproject.daoimpl.UserDAOImpl;
+import com.ppmtoolproject.domain.User;
+
+public class DAOSaveTest {
+
+	public static void main(String[] args) {
+		UserDAOImpl dao = new UserDAOImpl();
+		User u = new User();
+		u.setName("Adam");
+		u.setPassword("Adam");
+		u.setEmail("adam@adam.com");
+		u.setUserType("dummy");
+		
+		dao.save(u);
+	}
+
+}
