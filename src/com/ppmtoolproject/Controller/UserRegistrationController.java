6db253ee7c0fef09d0c816b94f38a1401b640d8b
Vivek Mishra <vivek.wap4@gmail.com>
2019-07-24 13:13:06 -0500

    Started post method of user registration

--- src/com/ppmtoolproject/Controller/UserRegistrationController.java
+++ src/com/ppmtoolproject/Controller/UserRegistrationController.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ppmtoolproject.dao.UserDAO;
+import com.ppmtoolproject.daoimpl.UserDAOImpl;
+import com.ppmtoolproject.domain.User;
+
 /**
  * Servlet implementation class UserRegistrationController
  */
 @WebServlet("/UserRegistrationController")
 public class UserRegistrationController extends HttpServlet {
+	UserDAO userDao = new UserDAOImpl();
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -24,10 +29,17 @@ public class UserRegistrationController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		User user = new User();
+		user.setName(request.getParameter("user_name"));
+		user.setEmail(request.getParameter("user_email"));
+		user.setPassword(request.getParameter("user_password"));
+		user.setUserType(request.getParameter("user_type"));
 		doGet(request, response);
 		
 		//Now use the request.getParameter() method to get the form parameters from the request. This method is invoked after user
 		// clicks submit on the registration form
+		
+		
 		
 	}
 
